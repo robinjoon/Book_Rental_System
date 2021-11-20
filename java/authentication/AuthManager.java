4a580b148604325e0da2e0ca0adaeff79ffa6db7
robinjoon <0108dlatnqls@gmail.com>
2021-11-20 17:22:07 +0900

    add AuthManager

---
+++ java/authentication/AuthManager.java
@@ -0,0 +1,14 @@
+package authentication;
+
+public class AuthManager {
+
+	public Object createJwt(Object info) {
+		return null;
+	}
+	public Object verifyJwt(Object jwt) {
+		return null;
+	}
+	public Object updateJwt() {
+		return null;
+	}
+}
