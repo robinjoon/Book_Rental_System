52dcd0871188d74ce4d6f9d2190b46129e3ea04c
robinjoon <0108dlatnqls@gmail.com>
2021-11-20 17:01:33 +0900

    add Spring Config Files

---
+++ java/config/ControllerConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ControllerConfig {
+
+}
