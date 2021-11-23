9d7cfc47b3c43b433ef08bc3078b222d0ace8536
robinjoon <0108dlatnqls@gmail.com>
2021-11-23 20:38:12 +0900

    implement MemberService

--- java/services/MemberService.java
+++ java/services/MemberService.java
@@ -1,6 +1,10 @@
 package services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import dao.MemberDao;
 import dto.Member;
@@ -10,23 +14,45 @@ public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
 	
-	public Member registMember(Member member) {
+	public ResponseEntity<Member> registMember(Member member) {
 		if(memberDao.insertMember(member)) {
-			return member;
+			return ResponseEntity.ok(member); 
 		}else {
-			return new Member.Builder(null, null, null).build();
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		}
 	}
-	public Object getMyInfo() {
-		return null;
+	
+	public ResponseEntity<Member> getMyInfo(String email) {
+		Member member = memberDao.selectMember(email);
+		if(member == null)
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(member);
 	}
-	public boolean updateMyInfo() {
-		return true;
+	public ResponseEntity<Member> updateMyInfo(Member member) {
+		if(memberDao.updateMember(member)) {
+			Member getMember = memberDao.selectMember(member.getEmail());
+			if(getMember == null)
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();  // required fix http status code
+			else
+				return ResponseEntity.ok(member);
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
 	}
-	public Object getMemberList(){
-		return null;
+	public ResponseEntity<List<Member>> getMemberList(){
+		List<Member> memberList = memberDao.selectMembers();
+		if(memberList==null || memberList.isEmpty()) {
+			return ResponseEntity.status(500).build();
+		}else {
+			return ResponseEntity.ok(memberList);
+		}
 	}
-	public Object getMember() {
-		return null;
+	public ResponseEntity<Member> getMember(String email) {
+		Member member = memberDao.selectMember(email);
+		if(member == null)
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(member);
 	}
 }
