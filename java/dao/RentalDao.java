008e86d17742a5633cea7e27684f0167cf37d3d4
robinjoon <0108dlatnqls@gmail.com>
2021-11-28 19:23:30 +0900

    modify method's parameter

--- java/dao/RentalDao.java
+++ java/dao/RentalDao.java
@@ -4,11 +4,11 @@ import dto.*;
 import java.util.*;
 public interface RentalDao {
 	
-	boolean insertRental(Book book);
-	boolean updateRental(Book book); // 대출기간 수정
-	boolean patchRental(Book book); // 반납 처리
+	boolean insertRental(String userID, Book book);
+	boolean updateRental(String userID, Book book); // 대출기간 수정
+	boolean patchRental(String userID, Book book); // 반납 처리
 	List<Rental> selectRentals(int bookId);
-	List<Rental> selectRentals(String email);
+	List<Rental> selectRentals(String userID);
 	List<Rental> selectRental(int rentalId);
 	
 }
