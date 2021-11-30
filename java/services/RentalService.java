0b554f9432d139e49cadf56831852ae6a82772d4
robinjoon <0108dlatnqls@gmail.com>
2021-11-30 21:47:18 +0900

    implements RentalService

--- java/services/RentalService.java
+++ java/services/RentalService.java
@@ -1,17 +1,36 @@
 package services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dao.RentalDao;
+import dto.Rental;
+
 public class RentalService {
 	
-	public boolean rentalBook() {
-		return true;
+	@Autowired
+	private RentalDao rentalDao;
+
+	public boolean rentalBook(Rental rental) {
+		return rentalDao.insertRental(rental);
 	}
-	public boolean returnBook() {
-		return true;
+	public boolean returnBook(Rental rental) {
+		return rentalDao.patchRental(rental);
 	}
 	public Object lostBook() {
 		return null;
 	}
-	public boolean updateRentalInfo(Object object) {
-		return true;
+	public boolean updateRentalInfo(Rental rental) {
+		return rentalDao.updateRental(rental);
+	}
+	public List<Rental> getRentalList(String user_id){
+		return rentalDao.selectRentals(user_id);
+	}
+	public List<Rental> getRentalList(int book_id){
+		return rentalDao.selectRentals(book_id);
+	}
+	public Rental getRental(int rental_id){
+		return rentalDao.selectRental(rental_id);
 	}
 }
