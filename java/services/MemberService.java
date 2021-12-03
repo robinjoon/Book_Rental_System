ed434b087c706cd4f459430d25aefe3f0b467b58
robinjoon <0108dlatnqls@gmail.com>
2021-12-03 21:14:24 +0900

    modify MemberService to satisfy the MVC pattern

--- java/services/MemberService.java
+++ java/services/MemberService.java
@@ -3,9 +3,6 @@ package services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import dao.MemberDao;
 import dto.Member;
 
@@ -14,45 +11,23 @@ public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
 	
-	public ResponseEntity<Member> registMember(Member member) {
-		if(memberDao.insertMember(member)) {
-			return ResponseEntity.ok(member); 
-		}else {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-		}
+	public boolean registMember(Member member) {
+		return memberDao.insertMember(member);
 	}
 	
-	public ResponseEntity<Member> getMyInfo(String email) {
-		Member member = memberDao.selectMember(email);
-		if(member == null)
-			return ResponseEntity.notFound().build();
-		else
-			return ResponseEntity.ok(member);
+	public Member getMyInfo(String userId) {
+		return memberDao.selectMember(userId);
 	}
-	public ResponseEntity<Member> updateMyInfo(Member member) {
-		if(memberDao.updateMember(member)) {
-			Member getMember = memberDao.selectMember(member.getId());
-			if(getMember == null)
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();  // required fix http status code
-			else
-				return ResponseEntity.ok(member);
-		}else {
-			return ResponseEntity.badRequest().build();
-		}
+	
+	public boolean updateMyInfo(Member member) {
+		return memberDao.updateMember(member);
 	}
-	public ResponseEntity<List<Member>> getMemberList(){
-		List<Member> memberList = memberDao.selectMembers();
-		if(memberList==null || memberList.isEmpty()) {
-			return ResponseEntity.status(500).build();
-		}else {
-			return ResponseEntity.ok(memberList);
-		}
+	
+	public List<Member> getMemberList(){
+		return memberDao.selectMembers();
 	}
-	public ResponseEntity<Member> getMember(String email) {
-		Member member = memberDao.selectMember(email);
-		if(member == null)
-			return ResponseEntity.notFound().build();
-		else
-			return ResponseEntity.ok(member);
+	
+	public Member getMember(String userId) {
+		return memberDao.selectMember(userId);
 	}
 }
