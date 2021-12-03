b59a6ce950320c0bdcc62ad0990f4a1d01701b58
robinjoon <0108dlatnqls@gmail.com>
2021-12-03 21:15:09 +0900

    create MemberController

--- java/controllers/MemberController.java
+++ java/controllers/MemberController.java
@@ -1,22 +1,57 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import dto.Member;
 import services.MemberService;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
 	
-	@PostMapping("/regist")
-	private Member registMember(@RequestBody Member member) {
-		return memberService.registMember(member);
+	@PostMapping("")
+	private ResponseEntity<?> registMember(@RequestBody Member member) {
+		if(memberService.registMember(member)) {
+			return ResponseEntity.ok().build();
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	@GetMapping("/{email}")
+	private ResponseEntity<Member> getMember(@PathVariable("email") String email) {
+		Member member = memberService.getMember(email);
+		if(member == null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			return ResponseEntity.ok(member);
+		}
+	}
+	
+	@GetMapping("")
+	private ResponseEntity<List<Member>> getMembers(){
+		List<Member> memberList = memberService.getMemberList();
+		if(memberList == null) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			return ResponseEntity.ok(memberList);
+		}
+		
+	}
+	@GetMapping("/myInfo")
+	private ResponseEntity<Member> getMyInfo(){
+		String userId = ""; // get userId from JWT
+		Member member = memberService.getMember(userId);
+		if(member == null) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			return ResponseEntity.ok(member);
+		}
 	}
 }
