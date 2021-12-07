1935c88ee01e19198f0df659743ea15eafa2dd72
robinjoon <0108dlatnqls@gmail.com>
2021-12-07 19:28:49 +0900

    modify BookDao.insertBook() return int

--- java/dao/BookDao.java
+++ java/dao/BookDao.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public interface BookDao {
 	
-	boolean insertBook(Book book);
+	int insertBook(Book book);
 	boolean deleteBook(int bookId);
 	List<Book> selectBooks();
 	List<Book> selectBooks(String category);
